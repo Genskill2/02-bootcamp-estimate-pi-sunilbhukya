b16779526deb38b33b443199f287828865ed08c5
Sunil Kumar <sunilbhukya994@gmail.com>
2021-06-02 16:38:44 +0530

    wrote program for estimation

--- estimate.py
+++ estimate.py
@@ -1,6 +1,31 @@
 import math
 import unittest
-
+import random
+def wallis(n):
+ c = 1
+ prod = 1
+ s = 1
+ while c<(n+1):
+  prod *= (4 * s ** 2 / (4 * s ** 2 - 1))
+  s = s+1
+  c = c+1
+  prod *= 2
+ return prod
+ 
+ 
+def monte_carlo(n):
+ count=0
+ for i in range(1,n):
+     x = random.random()
+     y = random.random()
+     d=(((x)**2)+((y)**2))**0.5
+     if d<1:
+       count=count+1
+ x=count/n
+ pi=x*4
+ return pi
+ 
+ 
 class TestWallis(unittest.TestCase):
     def test_low_iters(self):
         for i in range(0, 5):
